1573ca8d583346f8c5d3495b727bad0cd9ba4080
ArturMkrt <artur-mkrtchyan-2019@list.ru>
2020-04-08 22:15:18 +0400

    remove rE

--- removeElement.py
+++
@@ -1,9 +0,0 @@
-list_1 = [1,2,3,4]
-remove = [int(x) for x in input("remove element ").split()]
-if len(remove) == 1:
-    while remove[0] in list_1:
-        list_1.remove(remove[0])
-else:
-    while remove in list_1:
-        list_1.remove(remove)
-print(list_1)
