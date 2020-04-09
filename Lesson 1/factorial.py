f0289ef59e53326bce32ed8553a1b7db48317309
ArturMkrt <artur-mkrtchyan-2019@list.ru>
2020-04-09 20:25:19 +0400

    .

--- Lesson 1/factorial.py
+++ Lesson 1/factorial.py
@@ -1,12 +1,12 @@
-n = int(input())
-factorial = 1
-while n > 1:
-    factorial *= n
-    n -= 1
-print(factorial)
+def factorial(n):
+    factorial = 1
+    while n > 1:
+        factorial *= n
+        n -= 1
+print(factorial(5))
 
 def factorial(n):
-    if n == 1:
+    if n == 0:
         return 1
     return factorial(n - 1) * n
 print(factorial(5))
