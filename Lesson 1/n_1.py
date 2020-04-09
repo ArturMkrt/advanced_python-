6cca57afd2a1a96850fa01afb54f6678196e47de
ArturMkrt <artur-mkrtchyan-2019@list.ru>
2020-04-09 21:22:59 +0400

    change

--- Lesson 1/n_1.py
+++ Lesson 1/n_1.py
@@ -1,7 +1,6 @@
 def int_l(lst):
     for item in lst:
         if len(str(item)) == len(set(str(item))):
-            a = False
             return item
     return -1
 print(int_l([101, 110, 11, 111]))
