f0e83e4768f77fa3f40c50b81f0f17dd7f83a520
ArturMkrt <artur-mkrtchyan-2019@list.ru>
2020-04-11 00:46:20 +0400

    C:/Program Files/Git/

--- Lesson 1/contains.py
+++ Lesson 1/contains.py
@@ -5,6 +5,7 @@ def is_shifted(list1, list2):
     return str1 in str2*2
   return False
 
+
 print(is_shifted([1, 2, 3], [2, 3, 1]))
 print(is_shifted([1, 3, 4, 5, 2], [5, 2, 1, 3, 4]))
 print(is_shifted([1, 2, 3], [3, 2, 1]))
