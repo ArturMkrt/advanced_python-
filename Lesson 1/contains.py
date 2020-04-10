c66ee97d8f92cdc373c64f74ecc15882b9ff9711
ArturMkrt <artur-mkrtchyan-2019@list.ru>
2020-04-10 23:52:55 +0400

    C:/Program Files/Git/

--- Lesson 1/contains.py
+++ Lesson 1/contains.py
@@ -1,15 +1,9 @@
-def contains(list1, list2):
-    diff = len(list1) - len(list2)
-    if diff >= 0:
-        for i in range(diff):
-            if list1[i:i + len(list2)] == list2:
-                return True
-    return False
-
-
 def is_shifted(list1, list2):
-    return len(list1) == len(list2) and contains(list2 * 2, list1)
-
+  if len(list1) == len(list2):
+    str1 = "".join([str(i) for i in list1])
+    str2 = "".join([str(i) for i in list2])
+    return str1 in str2*2
+  return False
 
 print(is_shifted([1, 2, 3], [2, 3, 1]))
 print(is_shifted([1, 3, 4, 5, 2], [5, 2, 1, 3, 4]))
