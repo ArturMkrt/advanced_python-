9684bac2b437d6d39ab12cb68da4f4839165c2d6
ArturMkrt <artur-mkrtchyan-2019@list.ru>
2020-05-10 15:21:01 +0400

    C:/Program Files/Git/

---
+++ week4/practic4.py
@@ -0,0 +1,10 @@
+import pandas as pd
+group = {'Name':['Jora','Ruzanna','Hayk','Liana','Salbina','Sona','Tatev','Vlad','Nairi'],
+         'Surname':['Karyan','Ordyan','Sahakyan','Varosyan','Alaverdyan','Kirakosyan','Alaverdyan','Harutyunyan','Hakobyan'],   
+         'sex':['Male','Female','Male','Female','Female','Female','Female','Male','Male'],
+         'Age':[28,45,27,25,22,29,24,26,26],
+         'Status':['Student','Student','Student','Student','Student','Student','Student','Student','Tutor']
+        }
+df = pd.DataFrame(group,columns=['Name','Surname','sex','Age','Status'])
+df=  df.set_index('Name')
+print(df)
