a191527c47e516f4aa512040cf69e06b6e38ab71
ArturMkrt <artur-mkrtchyan-2019@list.ru>
2020-05-10 15:22:51 +0400

    C:/Program Files/Git/

--- week4/practic4.py
+++ week4/practic4.py
@@ -2,7 +2,7 @@ import pandas as pd
 group = {'Name':['Jora','Ruzanna','Hayk','Liana','Salbina','Sona','Tatev','Vlad','Nairi'],
          'Surname':['Karyan','Ordyan','Sahakyan','Varosyan','Alaverdyan','Kirakosyan','Alaverdyan','Harutyunyan','Hakobyan'],   
          'sex':['Male','Female','Male','Female','Female','Female','Female','Male','Male'],
-         'Age':[28,45,27,25,22,29,24,26,26],
+         'Age':[28,45,27,25,22,29,20,26,26],
          'Status':['Student','Student','Student','Student','Student','Student','Student','Student','Tutor']
         }
 df = pd.DataFrame(group,columns=['Name','Surname','sex','Age','Status'])
